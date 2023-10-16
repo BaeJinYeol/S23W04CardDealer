6bbdb637f5e833c216973c2338a8b4e31d5938c5
BaeJinYeol <qowlsduf4488@gmail.com>
2023-10-16 14:51:53 +0900

    add btn

--- app/src/main/java/kr/ac/kumoh/s20190558/s23w04carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20190558/s23w04carddealer/MainActivity.kt
@@ -2,10 +2,13 @@ package kr.ac.kumoh.s20190558.s23w04carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import kr.ac.kumoh.s20190558.s23w04carddealer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var main : ActivityMainBinding
+    private lateinit var main: ActivityMainBinding
+    private lateinit var model: CardDealerViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,14 +17,27 @@ class MainActivity : AppCompatActivity() {
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
 
-        main.card1.setImageResource(R.drawable.c_black_joker)
+        model = ViewModelProvider(this)[CardDealerViewModel::class.java]
+        // !!는 널이 될수가 절대 없다는 걸 알려줌
+        model.cards.observe(this, Observer {
+            val res = IntArray(5)
+            for (i in res.indices) {
+                res[i] = resources.getIdentifier(
+                    getCardName(it[i]), // model.cards.values!![i]
+                    "drawable",
+                    packageName
+                )
+            }
+            main.card1.setImageResource(res[0])
+        })
 
-        val res = resources.getIdentifier(getCardName(37), "drawable", packageName)
-        main.card1.setImageResource(res)
+        main.btnShuffle.setOnClickListener {
+            model.shuffle()
+        }
 
     }
     private fun getCardName(c: Int): String { // TODO: int형으로 반환 할 것
-        val shape = when (c / 13) {
+        var shape = when (c / 13) {
             0 -> "spades"
             1 -> "diamonds"
             2 -> "hearts"
@@ -32,15 +48,25 @@ class MainActivity : AppCompatActivity() {
         val number = when (c % 13) {
             0 -> "ace"
             in 1..9 -> (c % 13 + 1).toString()
-            10 -> "jack"
-            11 -> "queen"
-            12 -> "king"
+            10 -> {
+                shape = shape.plus("2")
+                "jack"
+            }
+            11 -> {
+                shape = shape.plus("2")
+                "queen"
+            }
+            12 -> {
+                shape = shape.plus("2")
+                "king"
+            }
             else -> "error"
         }
-        return if (number in arrayOf("jack", "queen", "king"))
-            "c_${number}_of_${shape}2"
-        else
-            "c_${number}_of_${shape}"
+//        return if (number in arrayOf("jack", "queen", "king"))
+//            "c_${number}_of_${shape}2"
+//        else
+//            "c_${number}_of_${shape}"
+        return "c_${number}_of_${shape}"
     }
 
 }
