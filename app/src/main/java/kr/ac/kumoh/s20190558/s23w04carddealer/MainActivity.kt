d85f99ec6629ee2f9eeeb0edd6680d0a680cbf5c
BaeJinYeol <qowlsduf4488@gmail.com>
2023-10-23 00:15:28 +0900

    풀 하우스 만 구현 하면 끝, 근데 앱 실행이 안됨...

--- app/src/main/java/kr/ac/kumoh/s20190558/s23w04carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20190558/s23w04carddealer/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.ac.kumoh.s20190558.s23w04carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -23,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         model.cards.observe(this, Observer {
             val res = IntArray(5)
             for (i in res.indices) {
-                val resourceId = resources.getIdentifier(
+                res[i] = resources.getIdentifier(
                     getCardName(it[i]), "drawable", packageName
                 )
                 val cardView: ImageView? = when (i) {
@@ -34,11 +35,11 @@ class MainActivity : AppCompatActivity() {
                     4 -> main.card5
                     else -> null
                 }
-                cardView?.setImageResource(resourceId)
+                Log.i("Card NUM!!", "${res[i]} + ${i+1} + ${it[i]}")
+                cardView?.setImageResource(res[i])
             }
-            main.txtRank.text = getPedigree(res)
+            main.txtRank.text = getPedigree(it)
         })
-
         main.btnShuffle.setOnClickListener {
             model.shuffle()
         }
@@ -73,11 +74,99 @@ class MainActivity : AppCompatActivity() {
         return "c_${number}_of_${shape}"
     }
     private fun getPedigree(deck: IntArray): String {
-        var pedigree = "No Hand"
-        val cardArray = IntArray(13) {0}
-        val patternArray = IntArray(4) {0}
+        val cardMatrix = Array(4) { Array(13) { 0 } }
 
+        for (cardNum in deck) {
+            val cPattern = cardNum / 13
+            val cNum = cardNum % 13
+            cardMatrix[cPattern][cNum]++
+        }
 
-        return pedigree
+        // 로얄 스트레이트 플러쉬
+        for (i in 0..3) {
+            if (cardMatrix[i][8] == 1 && cardMatrix[i][9] == 1 && cardMatrix[i][10] == 1 &&
+                cardMatrix[i][11] == 1 && cardMatrix[i][12] == 1){
+                return "Royal Straight Flush"
+            }
+        }
+        // 스트레이트 플러쉬
+        for (i in 0..3) {
+            for (j in 1..8) {
+                if (cardMatrix[i][j] == 1 && cardMatrix[i][j+1] == 1 && cardMatrix[i][j+2] == 1 &&
+                    cardMatrix[i][j+3] == 1 && cardMatrix[i][j+4] == 1) {
+                    return "Straight Flush"
+                }
+            }
+        }
+        // 포카드
+        for (j in 0..12) {
+            var count = 0
+            for (i in 0..3){
+                if (cardMatrix[i][j] == 1) count++
+            }
+            if (count >= 4) {
+                return "Four of a Kind"
+            }
+        }
+        // 풀 하우스
+
+        // 플러쉬 (같은 무늬 5장)
+        for (i in 0..3) {
+            var count = 0
+            for (j in 0..12){
+                if (cardMatrix[i][j] == 1) count++
+            }
+            if (count >= 5) {
+                return "Flush"
+            }
+        }
+        // 스트레이트
+        val uniqueNumbers: IntArray = IntArray(5)
+        for (i in deck.indices) {
+            uniqueNumbers[i] = deck[i] % 13
+        }
+        uniqueNumbers.sort()
+        for (i in 0..8) {
+            if (uniqueNumbers[0] == i && uniqueNumbers[1] == i + 1 && uniqueNumbers[2] == i + 2 &&
+                uniqueNumbers[3] == i + 3 && uniqueNumbers[4] == i + 4) {
+                return "Straight"
+            }
+        }
+        // 쓰리 오브 어 카인드, 트립스, 세트
+        for (j in 0..12) {
+            var count = 0
+            for (i in 0..3){
+                if (cardMatrix[i][j] == 1) count++
+            }
+            if (count >= 3) {
+                return "Three of a Kind"
+            }
+        }
+        // 투 페어
+        var onePair = false
+        for (j in 0..12) {
+            var count = 0
+            for (i in 0..3){
+                if (cardMatrix[i][j] == 1) count++
+            }
+            if (onePair && count >= 2){
+                return "Two Pairs"
+            }
+            if (count >= 2) {
+                onePair = true
+            }
+        }
+        // 원 페어
+        for (j in 0..12) {
+            var count = 0
+            for (i in 0..3){
+                if (cardMatrix[i][j] == 1) count++
+            }
+            if (count >= 2) {
+                return "One Pair"
+            }
+        }
+        // 노페어 High Card
+        return "No Pairs"
     }
 }
